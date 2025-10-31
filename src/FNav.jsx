1fe121a7037b85220b8f63834a101ed5c3d4b514
Yamansaini0405 <sainiyaman14@gmail.com>
2025-11-01 02:54:30 +0530

    fix: typo error

--- src/FNav.jsx
+++ src/FNav.jsx
@@ -213,7 +213,7 @@ const FNav = () => {
                         className="absolute inset-0 bg-gradient-to-r from-transparent via-white/20 to-transparent 
                       translate-x-[-100%] group-hover:translate-x-[100%] transition-transform duration-[2000ms]"
                       />
-                      <span className="relative z-10">Dashboad</span>
+                      <span className="relative z-10">Dashboard</span>
                     </button>
                   </Link>}
               </div>
